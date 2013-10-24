0753ea747f23ace25a6e0e01a20a9f5705753d73
danwinkler <danthemanwinkler@gmail.com>
2013-10-24 15:01:12 -0500

    added latest_notes function in trigger_helper

--- server/triggers/trigger_helper.py
+++ server/triggers/trigger_helper.py
@@ -25,9 +25,21 @@ def latest_node_data( node, name, time_span ):
 		data.append( { "time": row['time'], "value": json.loads( row['value'] ) } )
 	return data
 
+def latest_notes( node, time_span ):
+	conn = sqlite3.connect( "db.db" )
+	conn.row_factory = sqlite3.Row
+	db = conn.cursor()
+	time_ago = time.time() - time_span
+	rows = db.execute('SELECT note, time from notes where node=? AND time >= ?', (node,name,time_ago) ).fetchall()
+	conn.close()
+	data = []
+	for row in rows:
+		data.append( { "time": row['time'], "note": row['note'] } )
+	return data
+
 def make_alert( name, value, level ):
 	conn = sqlite3.connect( "db.db" )
 	db = conn.cursor()
 	rows = db.execute('INSERT INTO alerts (name,value,level,time) VALUES (?,?,?,?)', (name,value,level,time.time()) )
 	conn.commit()
-	conn.close()
+	conn.close()	
